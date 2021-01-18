f2c1cf03c771c953752152554d7adf7f10c8925a
Shushrruth <73961321+Shushrruth@users.noreply.github.com>
2021-01-18 19:41:47 +0530

    Add files via upload

---
+++ danq_jaspar.py
@@ -0,0 +1,51 @@
+
+'''
+
+DanQ
+Model architecture
+
+
+'''
+
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+import torch.nn.functional as F
+from torch.nn import MaxPool1d
+
+class Net(nn.Module):
+    def __init__(self, in_channels=4, out_channels=320):
+        super(Net, self).__init__()
+        self.Conv = nn.Conv1d(in_channels = 4, out_channels = 1024, kernel_size = 30)
+        self.Maxpool = nn.MaxPool1d(kernel_size = 11, stride = 15)
+        self.Drop = nn.Dropout(0.1)
+        self.BiLSTM = nn.LSTM(input_size = 65, hidden_size = 512, num_layers=2,
+                                 batch_first = True,
+                                 dropout = 0.5,
+                                 bidirectional = True)
+    
+        self.Linear1 = nn.Linear(1024*512*2, 925)
+        self.Linear2 = nn.Linear(925, 919)
+        self.Linear3 = nn.Linear(919,1)
+
+    def forward(self, input):
+        x = self.Conv(input)
+        x = F.relu(x)
+        x = self.Maxpool(x)
+        x = self.Drop(x)
+        x,_ = self.BiLSTM(x)
+        x = torch.flatten(x,1)
+        x = self.Linear1(x)
+        x = F.relu(x)
+        x = self.Linear2(x)
+        x = F.relu(x)
+        x = self.Linear3(x)
+        x = torch.sigmoid(x)
+
+        return x 
+
+k=torch.rand(1,4,1000).cuda()
+Net().cuda()(k.float())
+
