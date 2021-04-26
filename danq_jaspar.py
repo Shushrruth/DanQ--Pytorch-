1bface984bcbdcaf4be52898a9199ed2205c38a9
Shushrruth <73961321+Shushrruth@users.noreply.github.com>
2021-04-26 21:00:53 +0530

    Update danq_jaspar.py

--- danq_jaspar.py
+++ danq_jaspar.py
@@ -19,30 +19,44 @@ class Net(nn.Module):
     def __init__(self, in_channels=4, out_channels=320):
         super(Net, self).__init__()
         self.Conv = nn.Conv1d(in_channels = 4, out_channels = 1024, kernel_size = 30)
+        
         self.Maxpool = nn.MaxPool1d(kernel_size = 11, stride = 15)
+        
         self.Drop = nn.Dropout(0.1)
-        self.BiLSTM = nn.LSTM(input_size = 65, hidden_size = 512, num_layers=2,
+        
+        self.BiLSTM = nn.LSTM(input_size = 1024, hidden_size = 512, num_layers=2,
                                  batch_first = True,
                                  dropout = 0.5,
                                  bidirectional = True)
     
-        self.Linear1 = nn.Linear(1024*512*2, 925)
+        self.Linear1 = nn.Linear(65*512*2, 925)
+        
         self.Linear2 = nn.Linear(925, 1)
         
     def forward(self, input):
         x = self.Conv(input)
+        
         x = F.relu(x)
+        
         x = self.Maxpool(x)
+        
         x = self.Drop(x)
+        
+        x = torch.transpose(x, 1, 2)
+        
         x,_ = self.BiLSTM(x)
+        
         x = torch.flatten(x,1)
+        
         x = self.Linear1(x)
+        
         x = F.relu(x)
+        
         x = self.Linear2(x)
+        
         x = torch.sigmoid(x)
 
         return x 
 
 k=torch.rand(1,4,1000).cuda()
 Net().cuda()(k.float())
-
